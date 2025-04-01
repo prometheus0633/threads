3f3231244cf5f126c7747f7272430de83df745ee
prometheus0633 <obouchou@DESKTOP-FI23K60.localdomain>
2025-04-01 19:18:40 +0000

    mutexes, get the return values, multithreads

---
+++ multi_threads._prob.c
@@ -0,0 +1,30 @@
+# include "tr.h"
+
+int primes[10] = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 };
+
+void* routine(void* arg) {
+    sleep(1);
+    int index = *(int*)arg;
+    printf("%d ", primes[index]);
+    //free(arg);
+}
+
+int main(int argc, char* argv[]) {
+    pthread_t th[10];
+    int i;
+    for (i = 0; i < 10; i++) {
+        // int* a = malloc(sizeof(int));
+        // *a = i;
+        if (pthread_create(&th[i], NULL, &routine, &i) != 0) {
+            perror("Failed to created thread");
+        }
+        usleep(1000);
+    }
+    for (i = 0; i < 10; i++) {
+        if (pthread_join(th[i], NULL) != 0) {
+            perror("Failed to join thread");
+        }
+    }
+    
+    return 0;
+}
